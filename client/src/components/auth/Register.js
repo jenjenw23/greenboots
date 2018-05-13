5b30cedef2d66644688ecfe1e7addcdbf43d6b7f
Tom LaChance <guitarlanguageseries@gmail.com>
2018-05-12 19:28:52 -0500

    detailed Login.js and Register.js files.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+// import axios from "axios";
 
 class Register extends Component {
   // Each field has to have its own state within the component
   constructor() {
-      super();
-      this.state = {
-          name: '',
-          email: '',
-          password: '',
-          password2: '',
-          errors: {}
-      };
-      this.onChange = this.onChange.bind(this);
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
   }
   // Whenever the user types if fires off an onChange function
   // this changes the components state
   // e.target.name` whats the point? if its name it will be the name field, if its email it will be email. and so on
   // e.target.value` whats the point? whatever the user types we get the value
   onChange(e) {
-      this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
   }
   render() {
     return (
-        <div className="register">
+      <div className="register">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -30,36 +42,47 @@ class Register extends Component {
               <p className="lead text-center">Create your Greenboots account</p>
               <form action="create-profile.html">
                 <div className="form-group">
-                  <input type="text" className="form-control form-control-lg" 
-                  placeholder="Name" 
-                  name="name"
-                  value={this.state.name}
-                  onChange={this.onChange}
-                />
-                </div>
-                <div className="form-group">
-                  <input type="email" className="form-control form-control-lg" 
-                  placeholder="Email Address" 
-                  name="email" 
-                  value={this.state.email}
-                  onChange={this.onChange}
-                  />
-                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
-                </div>
-                <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" 
-                  placeholder="Password" 
-                  name="password" 
-                  value={this.state.password}
-                  onChange={this.onChange}
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" 
-                  placeholder="Confirm Password" 
-                  name="password2" 
-                  value={this.state.password2}
-                  onChange={this.onChange}
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.onChange}
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
@@ -68,8 +91,7 @@ class Register extends Component {
           </div>
         </div>
       </div>
-    
-    )
+    );
   }
 }
 export default Register;
