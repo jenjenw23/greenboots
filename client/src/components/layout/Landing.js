cb7391e5bd2ec7cd4deee70504939dadad4bd321
Tom LaChance <guitarlanguageseries@gmail.com>
2018-05-13 21:55:33 -0500

    we are now able to login and logout via binding to auth state. added componentDidMount() to Landing and login.

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,26 +1,49 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+//Why PropTypes? we're mapping state to props meaning we are going to have a property
+//whenever you have properties in your components you should have PropTypes
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
 
 class Landing extends Component {
+  //prevent people from typing in /login when they are already logged in.  That would be weird
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
   render() {
     return (
-        <div className="landing">
+      <div className="landing">
         <div className="dark-overlay landing-inner text-light">
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">WELCOME
-                </h1>
-                <p className="lead"> Create a hiking profile/portfolio, share posts and connect with other hikers</p>
+                <h1 className="display-3 mb-4">WELCOME</h1>
+                <p className="lead">
+                  {" "}
+                  Create a hiking profile/portfolio, share posts and connect
+                  with other hikers
+                </p>
                 <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
-                <Link to="/login" className="btn btn-lg btn-light">Login</Link>
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
-export default Landing;
+Landing.PropTypes = {
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(mapStateToProps)(Landing);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,47 +1,100 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
 class Navbar extends Component {
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
   render() {
-    return (
-      <nav className="navbar navbar-expand-lg py-3 fixed-top navbar-dark bg-dark">
-    <div className="container">
-    <Link className="navbar-brand" to="/">
-    
-    <img src="/img/logo.png" className="d-inline-block align-top logo" width="100%" alt="Greenboots logo"></img>
-    </Link>  
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="mobile-nav">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/profiles"> Hiker Profiles
-            {' '}
-            </Link>
-          </li>
-        </ul>
-
-        <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <a className="nav-item nav-link" href="/trails">Popular Trails</a>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
+    const { isAuthenticated, user } = this.props.auth;
+    //authLinks and guestLinks allow us to bind and be contingent on auth state ternary operator on line 91
+    const authLinks = (
+      <ul className="navbar-nav mr-auto">
+        {/*<li className="nav-item">
+          <Link className="nav-link" to="/register">
             Sign Up
-            </Link>
-          </li>
-          <li className="nav-item">
+          </Link>
+    </li>*/}
+        <li className="nav-item">
+          <a
+            href=""
+            onClick={this.onLogoutClick.bind(this)}
+            className="nav-item nav-link"
+          >
+            <img
+              className="rounded-circle"
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: "25px", marginRight: "5px" }}
+              title="You must have a Gravatar connected to your email to display an image"
+            />{" "}
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
+    const guestLinks = (
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">
+            Sign Up
+          </Link>
+        </li>
+        <li className="nav-item">
           <Link className="nav-link" to="/login">
             Login
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-    )
+          </Link>
+        </li>
+      </ul>
+    );
+
+    return (
+      <nav className="navbar navbar-expand-lg py-3 fixed-top navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <img
+              src="/img/logo.png"
+              className="d-inline-block align-top logo"
+              width="100%"
+              alt="Greenboots logo"
+            />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/profiles">
+                  {" "}
+                  Hiker Profiles{" "}
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-item nav-link" href="/trails">
+                  Popular Trails
+                </a>
+              </li>
+              {isAuthenticated ? authLinks : guestLinks}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 
@@ -49,7 +102,7 @@ class Navbar extends Component {
 //   <nav className="navbar navbar-expand-lg py-3 fixed-top navbar-dark bg-dark">
 //   <a className="navbar-brand" href="/">
 //     <img src="/assets/img/logo.png" className="d-inline-block align-top logo" width="100%" alt="Greenboots logo"></img>
-//   </a>  
+//   </a>
 //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 //     <span className="navbar-toggler-icon"></span>
 //   </button>
@@ -65,5 +118,13 @@ class Navbar extends Component {
 //   </div>
 // </nav>
 // );
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+//bring in auth state with mapStateToProps
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-export default Navbar;
+export default connect(mapStateToProps, { logoutUser })(Navbar);
