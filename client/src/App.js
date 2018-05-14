47175714b5173a8d1291ea6423618d524ba24c43
Shari Bratanch <sharibratanch@Sharis-MacBook-Air.local>
2018-05-13 21:04:08 -0500

    profile page components

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,17 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 
+// import Register from './components/auth/Register';
+// import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+
 class App extends Component {
   render() {
     return (
