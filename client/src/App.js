9889e71afe0c67b8b4c82fced840b89fb1e8dcbf
Woulas <jwoula01@presencehealth.net>
2018-05-15 08:58:56 -0500

    added search

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
 import NotFound from './components/not-found/NotFound';
+import SearchTrails from "./components/search/SearchTrails";
 
 import "./App.css";
 
@@ -60,6 +61,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/search-trails" component={SearchTrails} />
               <Route exact path="/profiles" component={Profiles} />
               <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
