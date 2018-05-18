282e5d5e0fc89e44cc08f1131669ac5d2adb81a8
Woulas <jwoula01@presencehealth.net>
2018-05-18 18:37:46 -0500

    deploy clean up

--- server.js
+++ server.js
@@ -32,6 +32,15 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+//for heroku
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
